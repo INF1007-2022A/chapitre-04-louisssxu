7c873d3a0952c325163b4f0a8944d2a709049442
Louis Xu <96323167+louisssxu@users.noreply.github.com>
2022-09-13 12:19:10 -0400

    exercice 2 resolve

--- exercice.py
+++ exercice.py
@@ -9,8 +9,7 @@ def is_even_len(string: str) -> bool:
 
 def remove_third_char(string: str) -> str:
     #Supprimer le 3ème caractère d’une chaîne de caractères
-    pass
-
+    return string[:2] + string[3:]
 
 def replace_char(string: str, old_char: str, new_char: str) -> str:
     pass
